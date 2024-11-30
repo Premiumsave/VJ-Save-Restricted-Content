d4bb015c6e56b69de948130b6a832cd12ed5c835
Premiumsave <ynapxdzxecmclyalc12345678@gmail.com>
2024-11-30 16:06:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "22473416"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "ca4a166df6f575a22497adcc1451cef6")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://ynapxdzxecmclyalc12345678:ylaXTzNUzamSWHP1@cluster0.mr6n1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
